794e1c6877d05f7c2b263ebabbd5e8fe2731b50b
robbinsj <robbins.j@husky.neu.edu>
2015-03-12 00:30:09 -0400

    unity 5

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    Rect startButton;
+    float buttonWidth;
+    float buttonHeight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        buttonHeight = Screen.height / 6;
+        buttonWidth = Screen.width / 6;
+        startButton = new Rect((Screen.width / 2) - (buttonWidth / 2), (Screen.height / 2) - (buttonHeight / 2), buttonWidth, buttonHeight);
+        if (GUI.Button(startButton, "Become a single mother with eight children!"))
+        {
+            Application.LoadLevel("test");
+        }
+    }
+}
